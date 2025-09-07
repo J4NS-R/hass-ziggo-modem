9285170f04d7c054e25227867387bae0f3572d25
Jans Rautenbach <14095706+J4NS-R@users.noreply.github.com>
2025-09-07 15:11:58 +0200

    https skip verify (#2)

--- custom_components/ziggo_modem/modem_api.py
+++ custom_components/ziggo_modem/modem_api.py
@@ -14,6 +14,7 @@ class ModemApiSession(requests.Session):
             'User-Agent': 'HomeAssistant/ziggo_modem',
             'Authorization': 'Bearer bad_token'
         })
+        self.verify = False
 
     def _refresh_token(self):
         resp = requests.post(self._login_url, json={'password': self._router_password}, headers={
@@ -37,7 +38,7 @@ class ModemApiSession(requests.Session):
 
 class ModemApi:
     def __init__(self, default_gateway: str, router_password: str):
-        self.api_base_url = f'http://{default_gateway}/rest/v1'
+        self.api_base_url = f'https://{default_gateway}/rest/v1'
         self.session = ModemApiSession(router_password, self.api_base_url+'/user/login')
         LOGGER.debug('Configured ModemAPI with base URL: %s', self.api_base_url)
 
